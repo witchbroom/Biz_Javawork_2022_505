f06e56923b989dfa482d746e63b507f7243eef37
aintbine@gmail.com <aintbine@gmail.com>
2022-04-27 15:27:15 +0900

    java

---
+++ Java_50_App_03/src/com/callor/app/exec/Exec5.java
@@ -0,0 +1,12 @@
+package com.callor.app.exec;
+
+public class Exec5 {
+	
+	public static void main(String[] args) {
+		
+		int num = (int)(Math.random() * 100) + 1;
+		
+		
+	}
+
+}
---
+++ Java_50_App_03/src/com/callor/app/exec/Exec6.java
@@ -0,0 +1,13 @@
+package com.callor.app.exec;
+
+public class Exec6 {
+	
+	public static void main(String[] args) {
+		
+		int num = (int)(Math.random() * 100) + 1;
+		
+		int index = 0;
+		
+	}
+
+}
