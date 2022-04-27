c4f2da53b8ab5283816413f19fbc92f1a7b8dfc2
aintbine@gmail.com <aintbine@gmail.com>
2022-04-27 11:11:36 +0900

    java

--- Java_50_App_03/src/com/callor/app/exec/Exec4.java
+++ Java_50_App_03/src/com/callor/app/exec/Exec4.java
@@ -14,24 +14,24 @@ public class Exec4 {
 		// 한번이라도 0으로 나누어떨어지면 그것은 합성수이고, 그렇지 않은 경우는 소수라는 결론
 
 		// 소수를 판별하는 조건문 선언
-		boolean pNum = false;
+		boolean pNum = true;
 		
 		// 1은 소수도 합성수도 아니고, 2인 경우에는 반복문이 실행되지 않기 때문에 2를 기본값으로 설정한다.
-		// 1과 자기자신(num) 외에 다른 수로 나누어떨어질 경우(num % 1 == 0 이 true일 경우) 합성수이다.
+		// 1과 자기자신(num) 외에 다른 수로 나누어떨어질 경우(num % 1 == 0 이 false일 경우) 합성수이다.
 		// 합성수를 구했으니 break를 사용하여 반복문에서 빠져나온다.
 		
 		
 		for(int i = 2 ; i < num ; i++) {
 			if(num % i == 0) {
-				pNum = true;
+				pNum = false;
 				break;
 			}
 		}
 		
 		if(pNum) {
-			System.out.println(num + " 는 소수가 아니다.");
-		} else {
 			System.out.println(num + " 는 소수이다.");
+		} else {
+			System.out.println(num + " 는 소수가 아니다.");
 		}
 	}
 
